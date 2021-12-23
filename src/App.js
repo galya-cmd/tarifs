b0dc16152576515f0653170861b25f067f8bccfb
galya-cmd <galya1985@bk.ru>
2021-12-23 22:57:40 +0300

    изменения стиля

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Tarif from './components/Tarif.jsx';
 import './components/Tarif.css';
 
-const tarifs = [{name : "Безлимитный 300", color : 'grren', price : "300 руб./мес", speed :"до 10 Мбит/сек", text :"Обьем включенного тарифа не ограничен"},
+const tarifs = [{name : "Безлимитный 300", color : 'rgb(159, 238, 231)', price : "300 руб./мес", speed :"до 10 Мбит/сек", text :"Обьем включенного тарифа не ограничен"},
 { name :"Безлимитный 450",color : 'grey', price:"450 руб./мес" ,speed :"до 50 Мбит/сек", text:"Обьем включенного тарифа не ограничен"},
 {name:"Безлимитный 550", color : 'blue', price:"550 руб./мес" ,speed:"до 200 Мбит/сек" ,text:"Обьем включенного тарифа не ограничен"},
 {name:"Безлимитный 1000",color : 'red', price:"1000 руб./мес", speed:"до 10 Мбит/сек", text:"Обьем включенного тарифа не ограничен"}
