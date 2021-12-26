c9ab996fbf1f09c5ef8d2b772283cf20f7da3b3b
galya-cmd <galya1985@bk.ru>
2021-12-26 15:22:34 +0300

    добавила фон скорости тарифа

--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -15,7 +15,7 @@ class Tarif extends React.Component {
 
         <div {...props} style={{ backgroundColor: color }}>{name}</div>
         <div  {...props} style={{ backgroundColor: color }}>{price}</div>
-        <div {...props} style={{ backgroundColor: color }}>{speed}</div>
+        <div {...props} style={{ backgroundColor: '#F5F5F5' }}>{speed}</div>
         <div {...props} style={{ backgroundColor: '#504b4b' }}>{text}</div>
       </div>
     );
