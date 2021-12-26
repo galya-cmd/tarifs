5ca2d7d6d118b8feff691cbac070a96c935f0900
galya-cmd <galya1985@bk.ru>
2021-12-26 15:20:58 +0300

    цвет фона

--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -3,23 +3,23 @@ import './Tarif.css';
 
 class Tarif extends React.Component {
 
-  render(){
+  render() {
     const name = this.props.name;
-  const {color,price, speed, text, ...props} = this.props;
-  
-  
-  
-    return(
-    
+    const { color, price, speed, text, ...props } = this.props;
+
+
+
+    return (
+
       <div className="cart">
-      
-      <div {...props} style={{backgroundColor: color}}>{name}</div>
-      <div  {...props}style={{backgroundColor: color}}>{price}</div>
-      <div {...props}>{speed}</div>
-      <div {...props}>{text}</div> 
+
+        <div {...props} style={{ backgroundColor: color }}>{name}</div>
+        <div  {...props} style={{ backgroundColor: color }}>{price}</div>
+        <div {...props} style={{ backgroundColor: color }}>{speed}</div>
+        <div {...props} style={{ backgroundColor: '#504b4b' }}>{text}</div>
       </div>
-      );
-    }  
+    );
+  }
 }
 
 export default Tarif;
