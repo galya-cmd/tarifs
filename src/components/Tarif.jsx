3faddbb43b956974febff0665fad7ff93fbf87e3
galya-cmd <galya1985@bk.ru>
2021-12-26 15:45:54 +0300

    ПРОбный вариант if

--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -8,16 +8,16 @@ class Tarif extends React.Component {
   render() {
     const name = this.props.name || "Тариф выбран!";
     const { color, price, speed, text, ...props } = this.props;
-let cart = document.querySelector('cart');
-    if (cart.onClick ){
-      return 'Тариф';
+    let out = document.getElementById('out');
+    let cart = document.getElementById('cart');
+    if(cart.onclick){
+      out.innerHTML = 'Тариф';
     }
-
     return (
 
       <div className="cart" id='cart'>
 
-        <div {...props} style={{ backgroundColor: color }}>{name}</div>
+        <div {...props} id ='out'style={{ backgroundColor: color }}>{name}</div>
         <div  {...props} style={{ backgroundColor: color }}>{price}</div>
         <div {...props} style={{ backgroundColor: '#F5F5F5' }}>{speed}</div>
         <div {...props} style={{ backgroundColor: '#504b4b' }}>{text}</div>
