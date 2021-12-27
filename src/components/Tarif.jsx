32b82826041ae5b9bf30a958bdf2d339e64f8f02
galya-cmd <galya1985@bk.ru>
2021-12-27 23:18:17 +0300

    state

--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -3,23 +3,23 @@ import './Tarif.css';
 
 class Tarif extends React.Component {
   state = {
-    name : "Тариф выбран!"
+    name : false
   };
 
   makeSelected = () => {
     this.setState({
-      name : 'Ok'
+      name : !this.state.name
     });
   }
   render() {
-    
-    const {name, color, price, speed, text, ...props } = this.props;
+    const name = this.state.name;
+    const {color, price, speed, text, ...props } = this.props;
  
     return (
 
       <div className="cart" id='cart' onClick={this.makeSelected} >
 
-        <div {...props} id ='out'style={{ backgroundColor: color }}>{name}</div>
+        <div {...props} id ='out'style={{ backgroundColor: color }}>{this.state.name ? "Нажата" : "Нажми меня"}</div>
         <div  {...props} style={{ backgroundColor: color }}>{price}</div>
         <div {...props} style={{ backgroundColor: '#F5F5F5' }}>{speed}</div>
         <div {...props} style={{ backgroundColor: '#504b4b' }}>{text}</div>
